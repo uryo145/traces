a15285c5b7fdc77e08881e871df6a88b4bf35c52
uryo <walking.ryo@gmail.com>
2013-08-30 07:37:05 +0900

    first commit

---
+++ rectangle.java
@@ -0,0 +1,20 @@
+class Rectangle {
+    int width;
+    int height;
+    void setSize(int w, int h) {
+	width = w;
+	height = h;
+    }
+    int getArea() {
+	return width * height;
+    }
+}
+
+public static voie main(String[] args) {
+    Rectangle r = new Rectangle();
+    r.width = 123;
+    r.height = 45;
+
+    System.out.println("幅は" + r.width);
+    Sytem.out.println("高さは" + r.height);
+}
