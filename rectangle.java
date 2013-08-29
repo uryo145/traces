09d0fbdebefca440c6d55b0254d992748a1304ef
uryo <walking.ryo@gmail.com>
2013-08-30 07:42:11 +0900

    Use constructor to initialize fields

--- rectangle.java
+++ rectangle.java
@@ -1,18 +1,20 @@
 class Rectangle {
     int width;
     int height;
-    void setSize(int w, int h) {
+
+    //constructor
+    Rectangle(int w, int h) {
 	width = w;
 	height = h;
     }
+
     int getArea() {
 	return width * height;
     }
 }
 
 public static voie main(String[] args) {
-    Rectangle r = new Rectangle();
-    r.setSize(123,45);
+    Rectangle r = new Rectangle(123,45);
 
     System.out.println("幅は" + r.width);
     Sytem.out.println("高さは" + r.height);
